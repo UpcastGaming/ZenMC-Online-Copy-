3b0e6478f47e4c81ef91d8d1c92c2289df42aa97
UpcastGaming <75406920+UpcastGaming@users.noreply.github.com>
2024-01-05 15:29:24 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+const ngrok = require("@ngrok/ngrok");
+require('dotenv').config()
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+(async function() {
+const listener = await ngrok.forward({ proto: 'tcp', addr: 25565, authtoken_from_env: true });
+const serverip = listener.url();
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    client.user.setActivity(`${serverip}`, { type: 4});
+    client.user.setStatus('dnd');
+    const channel = client.channels.cache.get('1036918993849684008');
+    channel.send(`Bot & Ngrok Online, Check my Status for the IP. \nRemove the tcp:// on the url \n\n${serverip}`);
+});})();
+client.login(token);
